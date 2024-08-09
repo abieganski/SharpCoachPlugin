dac2e4c67bfacf911d1cea9dc152d6570dcd7ff4
Adam Bieganski <abieganski@gmail.com>
2024-08-09 21:25:40 +0100

    bring back sample projects

---
+++ plugin_sample_projects/mapping/Mapping/Mapping/BLL/Models/Responses/UserResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Mapping.BLL.Models.Responses
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public bool IsActive { get; set; }
+        
+        public long[] StockIds { get; set; }
+        
+        public Stock OwnedStock { get; set; }
+
+        public class Stock
+        {
+            public int Id { get; set; }
+            
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ plugin_sample_projects/mapping/Mapping/Mapping/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Mapping.BLL.Models.Requests;
+using Mapping.BLL.Models.Responses;
+
+namespace Mapping
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private Response Convert(Request e)
+        {
+            return new Response() {Id = e.Id};
+        }
+
+    }
+
+    public class Request
+    {
+        public long Id { get; set; }
+    }
+
+    public class Response
+    {
+        public long Id { get; set; }
+    }
+}
